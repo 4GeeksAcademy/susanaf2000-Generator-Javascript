ba8bdd4125a169e1c0fc79182bef44aa181903d7
Susana Florêncio <101977568+susanaf2000@users.noreply.github.com>
2023-10-06 10:15:11 +0000

    change 1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateExcuse() {
+  let getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  let randomExcuse = `${getRandomItem(who)} ${getRandomItem(
+    action
+  )} ${getRandomItem(what)} ${getRandomItem(when)}`;
+  return randomExcuse;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseElement = document.getElementById("excuse");
+  let randomExcuse = generateExcuse(who, action, what, when);
+  excuseElement.innerHTML = randomExcuse;
 };
